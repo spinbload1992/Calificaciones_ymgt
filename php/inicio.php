ec80b011eedd6e6f64c09dd3cd5a52c36a09c0af
kbmendez <31286623+kbmendez@users.noreply.github.com>
2017-08-28 12:52:11 -0500

    inicio de la página
    creamos el inicio de la página

---
+++ php/inicio.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/clase2.css" rel="stylesheet">
+
+  </head>
+
+  <body style="background: mistyrose">
+    <header class="container-fluid">
+      <div class="container">
+         <h3 style="color:orange" class="text-center">Instituto Tecnológico de Querétaro</h3>
+        <h2 style="color:blue" class="text-center">Programación Web II</h2>
+       <br>
+        <h1 style="color:purple" class="text-center">- "B i e n v e n i d o" -</h1>
+
+
+      </div>
+    </header>
+    <div class="col-md-4 col-md-offset-4">
+      
+    </div>
+ <div class="container">
+    <div class="row">
+    <div class="col-md-16">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img align="center" src="imagenes/01.jpeg"  height="300" width="300" class="img img-responsive" HSPACE=150>
+    </div>
+
+    <div class="item" align="center">
+      <img align="center" src="imagenes/02.png"  height="300" width="300"  class="img img-responsive">
+    </div>
+
+    <div class="item" align="center">
+      <img align="center" src="imagenes/03.jpg"  height="300" width="300"  class="img img-responsive">
+    </div>
+  </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+  
+  </div>
+</div>
+<center>
+      <br>
+      <br>
+        <a href="http://localhost/BaseDatos/login.php" class="btn btn-default ">Login</a>
+      </center>
+
+</div>
+  
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    
+  </body>
+</html>
