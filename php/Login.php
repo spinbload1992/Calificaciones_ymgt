2b118485aa3d0b7dbb9d12d70534506dd88a3ab7
kbmendez <31286623+kbmendez@users.noreply.github.com>
2017-08-28 12:49:40 -0500

    login nuevo
    Modificamos el login

--- php/Login.php
+++ php/Login.php
@@ -1,39 +1,51 @@
-<<?php 
-<title>Inicia sesion</title>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4"></div>
-			<div class="col-md-4">
-				<form>
-					<label style="color: blue"><h4> Introduce el nombre del usuario</h4></label>
-					<input type="text" name="usuario" id="usuario" class="form-control" placeholder="Nombre">
-					<label style="color: blue"><h4>Contraseña</h4></label>
-					<input type="Password" id="password" name="Password" placeholder="Contraseña" class="form-control">
-					<br>
-					<br>
-					<div id="boton" onclick="datos()" class="btn btn-danger btn-block">Enviar</div>
-				</form>
-			</div>
-		</div>
-	</div>
-		<script type="text/javascript">
-		function datos()
-		{
-			var usuario=document.getElementById('usuario').value;
-			var password=document.getElementById('password').value;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LOGIN</title>
 
-			if (usuario=="Yair" && password==1234){
-				alert("Bienvenido");
-				window.location.href="http://www.google.com/";
-				}
-				else{
-					alert("Nombre del usuario o contraseña es incorrecto");
-					window.location.href="Login.php";
-		}
-		}
-	</script>	
-</head>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/clase2.css" rel="stylesheet">
+
+     </head>
+  <body background="imagenes/sss.jpg"> 
+
+  
+    <header class="container-fluid">
+      <div class="container">
+        <h1 style="color:white" class="text-center"> Programación Web II </h1> 
+      </div>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <form action="validar.php" method="POST">
+          <br>
+          <label style="color:white">Usuario</label>
+          <input type="text" class="form-control" name="usuario" placeholder="nombre de usuario">
+          <label style="color:white">Password:</label>
+          <input type="password" class="form-control" name="pass" placeholder="password">
+          <br>
+          <input type="submit" class="btn btn-primary btn-block" value="enviar">
+          </form>
+
+        </div>
+      </div>
+    </div>
+
+
+    <!-- aquí comienza el Formulario -->
+      
+
+  
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
- ?>
+  </body>
+</html>
