20f8dbd9f758db458805faba512ab166d49d1bce
spinbload1992 <31258910+spinbload1992@users.noreply.github.com>
2017-08-29 17:02:52 -0500

    reporte de calificaciones
    Avance en clase

--- php/reporte_calificaciones.php
+++ php/reporte_calificaciones.php
@@ -2,3 +2,13 @@
 require ("fpdf/fpdf.php");
 $con= mysql_i_conenect("localhost:8080","calif","web2","calificaciones")){
 echo "Error: No se pudo conectar a MySQL." . PHP_EOL. "<br>";
+echo "errno de depuración: " .mysqli_connect_errno() . PHP_EOL."<br>";
+echo "error de depuración: " .mysqli_connect_error() . PHP_EOL."<br>";
+}
+$consulta="SELECT usuario.nombre_largo, usuario.id_usuario, calificaciones.calificación, materia.descripcion from usuario, calificaciones, materia WHERE usuario.id_usuario0calificaciones.id_alumno AND calificaciones.id_materia=materia.id_materia";
+if ($sentencia = mysqli_prepare ($con, $consulta)){
+/*ejercutar la sentencia*/
+if (!mysqli_stmt_execute($sentencia)) echo "error 1";
+
+/**/
+
